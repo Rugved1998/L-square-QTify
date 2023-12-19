c92dbe011795b1a7aa7207042b3a7a51010ad6e4
Aradhika Ramesh <rugved.bmw@gmail.com>
2023-12-19 18:29:42 +0530

    session-6 Search and Button Component

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,13 @@
+import '../src/styles/Theme.css';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,11 @@ function App() {
         >
           Learn React
         </a>
+        <p> This is poopins Text</p>
       </header>
+      <p> This is poopins Text</p> */}
+      <SearchBar placheholder={"Search a album of your choice"}></SearchBar>
+      <Button> Give Feedback</Button>
     </div>
   );
 }
---
+++ qtify/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./SearchBar.module.css";
+import {ReactComponent as SearchIcon} from "../../assets/Search-icon.svg"
+
+function SearchBar({placeholder}){
+
+const formSubmit=(event)=>{
+event.preventDefault();
+}
+
+    return (<form className={styles.wrapper} onSubmit={formSubmit}>
+        <input className={styles.search}  type="Area" placeholder={placeholder} />
+        <button className={styles.searchButton} type="submit">Search </button>
+    </form>);
+}
+
+export default SearchBar;
